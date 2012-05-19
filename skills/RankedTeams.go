bd3e8fc97552eb447d9151b363d24c98eb91124b
Chris Hines <github@cs-guy.com>
2012-05-18 23:29:28 -0400

    Add RankedTeams.AddTeam(team, rank) method.

--- skills/RankedTeams.go
+++ skills/RankedTeams.go
@@ -16,6 +16,11 @@ func NewRankedTeams(teams []Team, ranks []int) *RankedTeams {
 	return &RankedTeams{teams, ranks}
 }
 
+func (rt *RankedTeams) AddTeam(team Team, rank int) {
+	rt.teams = append(rt.teams, team)
+	rt.ranks = append(rt.ranks, rank)
+}
+
 func (rt *RankedTeams) Len() int           { return len(rt.teams) }
 func (rt *RankedTeams) Less(i, j int) bool { return rt.ranks[i] < rt.ranks[j] }
 
