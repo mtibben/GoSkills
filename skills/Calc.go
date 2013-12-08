aa93e58a0effcd98bf5678a2d1ccd88d0149462e
Chris Hines <github@cs-guy.com>
2013-12-08 01:13:24 -0500

    Wrap long comments.

--- skills/Calc.go
+++ skills/Calc.go
@@ -14,9 +14,11 @@ const (
 
 // Methods required to calculate skills.
 type Calc interface {
-	// Calculates new ratings based on the prior ratings and team ranks; use 1 for first place, repeat the number for a tie (e.g. 1, 2, 2).
+	// Calculates new ratings based on the prior ratings and team ranks;
+	// use 1 for first place, repeat the number for a tie (e.g. 1, 2, 2).
 	CalcNewRatings(gi *GameInfo, priors []Team, teamRanks ...int) PlayerRatings
 
-	// Calculates the match quality as the likelihood of all teams drawing (0% = bad, 100% = well matched).
+	// Calculates the match quality as the likelihood of all teams
+	// drawing (0% = bad, 100% = well matched).
 	CalcMatchQual(gi *GameInfo, teams []Team) float64
 }
